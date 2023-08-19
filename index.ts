cf483f34d88019b788051c0f5e0e4cc77baba4d0
RioTheDev <87861408+RioTheDev@users.noreply.github.com>
2023-08-20 01:09:31 +0400

    test

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { SMTPServer } from "smtp-server";
 import { simpleParser } from "mailparser";
-
+import nodemailer from "nodemailer";
 const server = new SMTPServer({
   onData: (stream, session, callback) => {
     simpleParser(stream, {}, (err, parsed) => {
@@ -10,6 +10,27 @@ const server = new SMTPServer({
       stream.on("end", callback);
     });
   },
+  onAuth: (auth, session, callback) => {
+    callback(null);
+  },
+
   disabledCommands: ["AUTH"],
 });
+
 server.listen(25);
+const transport = nodemailer.createTransport({
+  host: "127.0.0.1",
+  port: 25,
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+const message = {
+  from: "from-example@email.com",
+  to: "gaming.bait1223@gmail.com",
+  subject: "Subject",
+  text: "Hello SMTP Email",
+};
+transport.sendMail(message, (err, info) => {
+  console.log(err);
+});
